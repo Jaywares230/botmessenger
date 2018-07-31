939d8bf0c086458e928336c614578cf2c6fe4ced
miguel <miguelsorinpro@gmail.com>
2018-07-31 13:44:00 +0200

    third save message predefined

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,7 +2,18 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
-
+Facebook::Messenger::Profile.set({
+  greeting: [
+    {
+      locale: 'default',
+      text: 'Welcome to your new bot overlord!'
+    },
+    {
+      locale: 'fr_FR',
+      text: 'Bienvenue dans le bot du Wagon !'
+    }
+  ]
+}, access_token: ENV['ACCESS_TOKEN'])
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
   message.reply(
