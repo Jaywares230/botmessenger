5f022a099dd65f0e0b1f04d7360e342492f1b3b1
miguel <miguelsorinpro@gmail.com>
2018-07-31 13:47:44 +0200

    third save menu

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,14 +3,43 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 Facebook::Messenger::Profile.set({
-  greeting: [
+  persistent_menu: [
     {
       locale: 'default',
-      text: 'Welcome to your new bot overlord!'
+      composer_input_disabled: true,
+      call_to_actions: [
+        {
+          title: 'My Account',
+          type: 'nested',
+          call_to_actions: [
+            {
+              title: 'What is a chatbot?',
+              type: 'postback',
+              payload: 'EXTERMINATE'
+            },
+            {
+              title: 'History',
+              type: 'postback',
+              payload: 'HISTORY_PAYLOAD'
+            },
+            {
+              title: 'Contact Info',
+              type: 'postback',
+              payload: 'CONTACT_INFO_PAYLOAD'
+            }
+          ]
+        },
+        {
+          type: 'web_url',
+          title: 'Get some help',
+          url: 'https://github.com/jgorset/facebook-messenger',
+          webview_height_ratio: 'full'
+        }
+      ]
     },
     {
-      locale: 'fr_FR',
-      text: 'Bienvenue dans le bot du Wagon !'
+      locale: 'zh_CN',
+      composer_input_disabled: false
     }
   ]
 }, access_token: ENV['ACCESS_TOKEN'])
