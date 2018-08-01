4cdecf433f98c5843572127b66484668a3539e70
miguel <miguelsorinpro@gmail.com>
2018-08-01 15:02:45 +0200

    menu buttons try 2

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,19 +5,6 @@ Dotenv.load
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
-Facebook::Messenger::Profile.set({
-  greeting: [
-    {
-      locale: 'default',
-      text: 'Welcome to your new bot overlord!'
-    },
-    {
-      locale: 'fr_FR',
-      text: 'Bienvenue dans le bot du Wagon !'
-    }
-  ]
-}, access_token: ENV['ACCESS_TOKEN'])
-
 
 Bot.on :message do |message|
   message.reply(text: 'Hello, human! #{ENV["ACCESS_TOKEN"]}')
